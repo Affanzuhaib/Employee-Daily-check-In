861189a8f9ac0d7f4b41a98887e4075faf524b57
Affanzuhaib <affanzuhaibuddin@gmail.com>
2023-06-09 22:02:55 +0530

    updated week 1 task

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -2,14 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    console.log(process.env.MONGO_URL)
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB connected`);
   } catch (error) {
     console.log(error);
     process.exit(1);
   }
-}; 
+};
 
 module.exports = connectDB;
-       
--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // app.use('/api/goals', require('./routes/routes'));
-app.use('/api/users', require('./routes/userRoutes')); 
+app.use('/api/users', require('./routes/userRoutes'));
 
-// app.use(errorHandler); 
+// app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
